350105f089253ca93925da78af75cce29d7a85e3
Mateus Alves <0daycode@protonmail.com>
2016-12-10 01:12:10 -0200

    Update test.rb

--- test.rb
+++ test.rb
@@ -5,8 +5,8 @@ b = 1000
 
 while a < b do 
 
-ip = "%d.%d.%d.%d" % [rand(256), rand(256), rand(256), rand(256)]
-puts ip
+$ip = "%d.%d.%d.%d" % [rand(250), rand(256), rand(256), rand(256)]
+puts ("IP: #$ip")
 a +=1
 
 end
