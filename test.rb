8bed88546671c7b9edbfb7bd90646e8e4d043fec
Mateus Alves <0daycode@protonmail.com>
2017-02-08 01:48:32 -0200

    Update test.rb

--- test.rb
+++ test.rb
@@ -25,14 +25,14 @@ puts ("\n")
      
 end
 
-options = {:generetor=> nil}
+options = {:generetor => nil}
 parser = OptionParser.new do|opts|
    
     opts.banner = "Exemple: ruby iphunder.rb -g 10 or --generetor 100 "
-    opts.on('-g ','--gem ', String)do |generetor|
+    opts.on('-g ','--gem ', Integer)do |generetor|
     options[:generetor]=generetor;
     end
-	banner()
+	
 	opts.on('-h', '--help', 'Help') do
        
         puts opts
@@ -51,9 +51,9 @@ green = "\033[01;32m"
 default = "\033[0m"
 banner()
 a = 0
-b = 25
 
-while a < b do 
+
+while a < (options[:generetor]) do 
 
 $ip = "%d.%d.%d.%d" % [rand(250), rand(256), rand(256), rand(256)]
 puts ("IP Random: #{green} [+] #$ip")
@@ -67,4 +67,6 @@ puts "DNS of IP: #{red} [+]  #{host_name}"
 end
 a +=1
 
+
 end
+puts "End"
